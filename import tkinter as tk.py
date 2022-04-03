c1253a436b1b3fd43054f05abb81ce031e9b4ee2
Reza <99068159@mydavinci.nl>
2022-04-03 20:09:26 +0200

    heb de opdracht gemaakt

---
+++ import tkinter as tk.py
@@ -0,0 +1,22 @@
+from cmath import exp
+from msilib.schema import Font
+from textwrap import fill
+import tkinter as tk
+
+HelloWindow = tk.Tk()
+
+HelloWindow.title("Hello")
+HelloWindow.geometry('380x200')
+HelloWindow.config(bg='grey')
+
+GreyLabel = tk.Label(HelloWindow, bg="grey").pack(ipady=10, fill='y', expand=True)
+
+HelloLabel = tk.Label(HelloWindow, text="Hello tkinter!", bg="green", fg="yellow")
+HelloLabel.pack(ipadx=75, ipady=50)
+
+Font_tuple = ("Comic Sans MS", 20, "bold")
+HelloLabel.configure(font=Font_tuple)
+
+GreyLabel = tk.Label(HelloWindow, bg="grey").pack(ipady=10, fill='y', expand=True)
+
+HelloWindow.mainloop()
