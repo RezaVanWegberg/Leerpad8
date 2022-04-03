0da41b64da3e9e431d0849ce66cb30a10af94111
Reza <99068159@mydavinci.nl>
2022-04-03 17:47:14 +0200

    ff een tussen commit voordat ik alles ga veranderen volgens de tutorial

--- GeometryManager.py
+++ GeometryManager.py
@@ -1,3 +1,7 @@
+from ast import With
+from distutils.command.config import config
+from operator import truediv
+from textwrap import fill
 import tkinter as tk
 
 
@@ -14,8 +18,8 @@ def Destroy():
     window.destroy()
 
 #box 1
-box1 = tk.Label(window,text="box 1")
-box1.pack(ipadx=20,ipady=10)
+box1 = tk.Label(window,text="box 1", bg='green', fg='white')
+box1.pack(ipadx=20,ipady=10, fill='both', expand=True)
 
 #box 2
 box2 = tk.Label(window, text="box 2")
@@ -23,9 +27,23 @@ box2.pack(ipadx=25,ipady=10)
 
 #top test
 for x in range(2):
-    window2 = tk.Toplevel()
-    box3 = tk.Label(window2, text="box")
-    box3.pack(ipadx=65,ipady=25)
-    button = tk.Button(window2, text="click", command=Destroy).pack(ipadx=10,ipady=10)
+    DestroyWindow = tk.Toplevel()
+    DestroyWindow.geometry('200x50')
+    # box3 = tk.Label(DestroyWindow, text="box")
+    # box3.pack(ipadx=60,ipady=25)
+    button = tk.Button(DestroyWindow, text="destroy", command=Destroy).pack(ipadx=10,ipady=10, fill='both', expand=True)
+
+# #top test2    alles van top test2 werkt niet, zodra ik de code actief heb wirdt de window gelijk withdrawed 
+# WindowDeiconify = tk.Toplevel()
+# WindowDeiconify.geometry('250x100')
+# button = tk.Button(WindowDeiconify, text="show", command= window.deiconify()).pack(ipadx=10,ipady=10, fill='both', expand=True)
+
+# WindowHide = tk.Toplevel()
+# WindowHide.geometry('250x100')
+# button2 = tk.Button(WindowHide, text="hide", command= window.withdraw()).pack(ipadx=10,ipady=10, fill='both', expand=True)
+
+
+
+
 
 window.mainloop()
